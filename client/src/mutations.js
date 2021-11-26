effbbbd9c37597a8cb196f58ad4555b0bf7844c9
Moradi1412 <moradi.javid@gmail.com>
2021-11-26 01:21:45 -0800

    added save and remove book

--- client/src/mutations.js
+++ client/src/mutations.js
@@ -23,3 +23,43 @@ export const ADD_USER = gql`
     }
   }
 `;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($input: savedBook!) {
+        saveBook (input: $input) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                # _id
+                bookId
+                authors
+                image
+                link
+                title
+                description
+            }
+        }
+    }
+`;
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: ID!) {
+        removeBook(bookId:$bookId) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                # _id
+                bookId
+                authors
+                image
+                link
+                title
+                description
+            }
+        }
+    }
+`;
