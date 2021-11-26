a70c07b9a5204b57cf4d795b2ce67463646c5441
Moradi1412 <moradi.javid@gmail.com>
2021-11-26 00:19:49 -0800

    typeDefs, resolvers and sever.js added apollo server

--- server/server.js
+++ server/server.js
@@ -2,9 +2,34 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+//import apollo 
+const { ApolloServer } = require ('apollo-server-express')
+
+//import typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//appllo Server
+const startServer = async () => {  
+  const server = new ApolloServer({ 
+    typeDefs, 
+    resolvers, 
+    context: authMiddleware 
+  });
+}; 
+
+//start the server 
+await server.start(); 
+//middleware 
+server.applyMiddleware({ app });
+// test log 
+console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+
+//init the apollo server
+startServer();
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
