204580efb25c3b8898bbba0548c51ce51c8566eb
Moradi1412 <moradi.javid@gmail.com>
2021-11-27 23:07:24 -0800

    bug

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -16,6 +16,34 @@ const SavedBooks = () => {
   // const loggedIn = Auth.loggedIn();
   const [removeBook, { error }] = useMutation(REMOVE_BOOK);
 
+    // // use this to determine if `useEffect()` hook needs to run again
+  // const userDataLength = Object.keys(userData).length;
+
+  // useEffect(() => {
+  //   const getUserData = async () => {
+  //     try {
+  //       const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+  //       if (!token) {
+  //         return false;
+  //       }
+
+  //       const response = await GET_ME(token);
+
+  //       if (!response.ok) {
+  //         throw new Error('something went wrong!');
+  //       }
+
+  //       const user = await response.json();
+  //       setUserData(user);
+  //     } catch (err) {
+  //       console.error(err);
+  //     }
+  //   };
+
+  //   getUserData();
+  // }, [userDataLength]);
+
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
     const token = Auth.loggedIn() ? Auth.getToken() : null;
@@ -30,6 +58,13 @@ const SavedBooks = () => {
       });
       // upon success, remove book's id from localStorage
       removeBookId(bookId);
+           // if (!response.ok) {
+      //   throw new Error('something went wrong!');
+      // }
+
+      // const updatedUser = await response.json();
+      // setUserData(updatedUser);
+    
       window.location.reload();
     } catch (err) {
       console.error(err);
